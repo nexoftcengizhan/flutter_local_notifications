ab7c0c998bf0b7c62178dbe15df6e356b9d49b6a
Cengizhan Parlak <cengizhanparlak@nexoftmobile.net>
2023-03-24 10:50:37 +0300

    hotfix: notifications crash when schedule mode comes null

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
@@ -19,34 +19,38 @@ import java.lang.reflect.Type;
 @Keep
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
 
-  @Override
-  @SuppressWarnings("deprecation")
-  public void onReceive(final Context context, Intent intent) {
-    String notificationDetailsJson =
-        intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
-    if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
-      // This logic is needed for apps that used the plugin prior to 0.3.4
-      Notification notification;
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-        notification = intent.getParcelableExtra("notification", Notification.class);
-      } else {
-        notification = intent.getParcelableExtra("notification");
-      }
-      notification.when = System.currentTimeMillis();
-      int notificationId = intent.getIntExtra("notification_id", 0);
-      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-      notificationManager.notify(notificationId, notification);
-      boolean repeat = intent.getBooleanExtra("repeat", false);
-      if (!repeat) {
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-      }
-    } else {
-      Gson gson = FlutterLocalNotificationsPlugin.buildGson();
-      Type type = new TypeToken<NotificationDetails>() {}.getType();
-      NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
-
-      FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
-      FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onReceive(final Context context, Intent intent) {
+        String notificationDetailsJson = intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
+        if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
+            // This logic is needed for apps that used the plugin prior to 0.3.4
+            Notification notification;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                notification = intent.getParcelableExtra("notification", Notification.class);
+            } else {
+                notification = intent.getParcelableExtra("notification");
+            }
+            notification.when = System.currentTimeMillis();
+            int notificationId = intent.getIntExtra("notification_id", 0);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(notificationId, notification);
+            boolean repeat = intent.getBooleanExtra("repeat", false);
+            if (!repeat) {
+                FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
+            }
+        } else {
+            Gson gson = FlutterLocalNotificationsPlugin.buildGson();
+            Type type = new TypeToken<NotificationDetails>() {
+            }.getType();
+            NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
+            if (notificationDetails.scheduleMode == null) {
+                Log.i("noticrash", "rescheduleNotifications: " + notificationDetails.scheduleMode);
+                notificationDetails.scheduleMode = ScheduleMode.exactAllowWhileIdle;
+                Log.i("noticrash", "rescheduleNotifications: " + notificationDetails.scheduleMode);
+            }
+            FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
+            FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
+        }
     }
-  }
 }
